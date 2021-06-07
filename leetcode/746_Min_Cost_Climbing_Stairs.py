bb5508824b511d573a356b869c1441a5957ae0be
Ivan <ivan.bazhenov333@gmail.com>
2021-06-07 13:41:12 +0300

    [LeetCode] : 746. Min Cost Climbing Stairs.

---
+++ leetcode/746_Min_Cost_Climbing_Stairs.py
@@ -0,0 +1,25 @@
+from math import inf
+from typing import List
+
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        l = len(cost)
+        i = 0
+
+        if l < 3:
+            return min(cost)
+
+        cost.extend([0, 0])
+        res = [inf] * (l + 2)
+        res[0], res[1] = cost[0], cost[1]
+
+        while i < l - 1:
+            a = res[i] + cost[i + 2]
+            b = res[i + 1] + cost[i + 2]
+            c = res[i + 1] + cost[i + 3]
+            res[i+2] = min(res[i+2], a, b)
+            res[i+3] = min(res[i+3], c)
+            i += 1
+
+        return res[-2]
