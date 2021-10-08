1071b3f02453510dd0e140f575db3812b484024d
Ivan <ivan.bazhenov333@gmail.com>
2021-10-08 23:00:29 +0300

    [AlgoExpert] : Depth-first Search.

---
+++ algoexpert/Depth-first_Search.py
@@ -0,0 +1,23 @@
+# Do not edit the class below except
+# for the depthFirstSearch method.
+# Feel free to add new properties
+# and methods to the class.
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def depthFirstSearch(self, array):
+        def dfs(t):
+            if t is None:
+                return
+            array.append(t.name)
+            for child in t.children:
+                dfs(child)
+
+        dfs(self)
+        return array
