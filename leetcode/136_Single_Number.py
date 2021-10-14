a6f38a99626044f75b72b625f63f9cb4b326a22c
Ivan <ivan.bazhenov333@gmail.com>
2021-10-14 19:37:00 +0300

    [LeetCode] : 136. Single Number.

---
+++ leetcode/136_Single_Number.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        s = set()
+        for i in nums:
+            if i in s:
+                s.remove(i)
+            else:
+                s.add(i)
+        for r in s:
+            return r
