616e54ebf447f8b62773f737f00174880764a872
Holmes555 <Ivan.bazhenov333@gmail.com>
2025-02-25 21:11:30 +0100

    [LeetCode] : 1524. Number of Sub-arrays With Odd Sum.

---
+++ leetcode/1524_Number_of_Sub-arrays_With_Odd_Sum.py
@@ -0,0 +1,19 @@
+﻿from typing import List
+
+
+class Solution:
+    def numOfSubarrays(self, arr: List[int]) -> int:
+        modulefinder = 10 ** 9 + 7
+        result = 0
+        number_of_odd = 0
+        number_of_even = 0
+        count = 0
+        for num in arr:
+            result += num
+            if result % 2 == 0:
+                count += number_of_odd
+                number_of_even += 1
+            else:
+                count += number_of_even + 1
+                number_of_odd += 1
+        return count % modulefinder
