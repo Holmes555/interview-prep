6b42a23eaa25645029e27ee59cec19ba6e63967f
Holmes555 <Ivan.bazhenov333@gmail.com>
2025-03-01 21:54:30 +0100

    [LeetCode] : 2460. Apply Operations to an Array.

---
+++ leetcode/2460_Apply_Operations_to_an_Array.py
@@ -0,0 +1,19 @@
+﻿from typing import List
+
+
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        l = len(nums)
+        result = [[],[]]
+        i = 0
+        while i < l:
+            if nums[i] == 0:
+                result[1].append(0)
+            elif i+1 < l and nums[i] == nums[i + 1]:
+                result[0].append(nums[i] * 2)
+                result[1].append(0)
+                i += 1
+            else:
+                result[0].append(nums[i])
+            i += 1
+        return result[0] + result[1]
