c30a0b43bd36a1e151c747a9d1bb58f5c937911c
Holmes555 <Ivan.bazhenov333@gmail.com>
2025-03-03 23:00:16 +0100

    [LeetCode] : 2161. Partition Array According to Given Pivot.

---
+++ leetcode/2161_Partition_Array_According_to_Given_Pivot.py
@@ -0,0 +1,16 @@
+﻿from typing import List
+
+
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        left_array = []
+        middle_array = []
+        right_array = []
+        for num in nums:
+            if num < pivot:
+                left_array.append(num)
+            elif num == pivot:
+                middle_array.append(num)
+            else:
+                right_array.append(num)
+        return left_array + middle_array + right_array
