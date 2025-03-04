9c368366bbaa2b9279ff8a73e882f2bd3ed87f25
Holmes555 <Ivan.bazhenov333@gmail.com>
2025-03-04 22:23:09 +0100

    [LeetCode] : 1780. Check if Number is a Sum of Powers of Three.

---
+++ leetcode/1780_Check_if_Number_is_a_Sum_of_Powers_of_Three.py
@@ -0,0 +1,18 @@
+﻿import math
+
+
+class Solution:
+    def __init__(self):
+        self.powers = set()
+
+    def checkPowersOfThree(self, n: int) -> bool:
+        power = math.log(n, 3)
+        if power in self.powers:
+            return False
+        if 3 ** round(power) == n:
+            return True
+        power = math.floor(power)
+        if power <= 0 or power in self.powers:
+            return False
+        self.powers.add(power)
+        return self.checkPowersOfThree(n - 3**power)
