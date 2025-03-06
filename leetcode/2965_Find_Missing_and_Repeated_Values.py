edd871cd8cef50c90fc4d2594b2e49b9067e6104
Holmes555 <Ivan.bazhenov333@gmail.com>
2025-03-06 19:19:08 +0100

    [LeetCode] : 2965. Find Missing and Repeated Values.

---
+++ leetcode/2965_Find_Missing_and_Repeated_Values.py
@@ -0,0 +1,20 @@
+﻿from typing import List
+
+
+class Solution:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        nums = set()
+        missing = -1
+        doubled = -1
+        for row in grid:
+            for num in row:
+                if num in nums:
+                    doubled = num
+                else:
+                    nums.add(num)
+        l = len(grid) ** 2
+        for i in range(1, l + 1):
+            if i not in nums:
+                missing = i
+                break
+        return [doubled, missing]
