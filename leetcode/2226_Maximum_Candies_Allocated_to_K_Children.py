4f993d7ad0e3a69b84d779c12c23a9de4d5b0120
Holmes555 <Ivan.bazhenov333@gmail.com>
2025-03-14 13:36:45 +0100

    [LeetCode] : 2226. Maximum Candies Allocated to K Children.

---
+++ leetcode/2226_Maximum_Candies_Allocated_to_K_Children.py
@@ -0,0 +1,31 @@
+﻿from typing import List
+
+
+class Solution:
+    def maximumCandies(self, candies: List[int], k: int) -> int:
+        sum_candies = sum(candies)
+        max_pile = max(candies)
+        max_candies = sum_candies // k
+        if max_candies == 0:
+            return 0
+
+        left = 1
+        right = min(max_pile, max_candies)
+        last_max = 1
+        while left < right:
+            mid = (left + right) // 2
+            piles = 0
+            for c in candies:
+                piles += c // mid
+            if piles < k:
+                right = mid
+            else:
+                last_max = mid
+                left = mid + 1
+
+        piles = 0
+        for c in candies:
+            piles += c // left
+        if piles >= k:
+            return left
+        return last_max
