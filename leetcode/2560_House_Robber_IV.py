f67d9fbc917e29d4b027b10095af87a04a508984
Holmes555 <Ivan.bazhenov333@gmail.com>
2025-03-15 18:59:06 +0100

    [LeetCode] : 2560. House Robber IV.

---
+++ leetcode/2560_House_Robber_IV.py
@@ -0,0 +1,26 @@
+﻿from typing import List
+
+
+class Solution:
+    def minCapability(self, nums: List[int], k: int) -> int:
+        left = min(nums)
+        right = max(nums)
+
+        while left < right:
+            mid = (left + right) // 2
+
+            i = 0
+            robberies = 0
+            while i < len(nums):
+                if nums[i] <= mid:
+                    i += 2
+                    robberies += 1
+                else:
+                    i += 1
+
+            if robberies >= k:
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
