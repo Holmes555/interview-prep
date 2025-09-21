d16aeafbcf9f54907cbec1f71c27ced5abe22688
Holmes555 <Ivan.bazhenov333@gmail.com>
2025-09-21 22:07:16 +0200

    [LeetCode] : 2596. Check Knight Tour Configuration.

---
+++ leetcode/2596_Check_Knight_Tour_Configuration.py
@@ -0,0 +1,20 @@
+﻿from typing import List
+
+
+class Solution:
+    def checkValidGrid(self, grid: List[List[int]]) -> bool:
+        n = len(grid)
+        if grid[0][0] != 0:
+            return False
+
+        moves = [(2, 1), (2, -1), (-2, 1), (-2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2)]
+
+        position = {grid[i][j]: (i, j) for i in range(n) for j in range(n)}
+
+        for i in range(n * n - 1):
+            x1, y1 = position[i]
+            x2, y2 = position[i + 1]
+            if (x2 - x1, y2 - y1) not in moves:
+                return False
+
+        return True
