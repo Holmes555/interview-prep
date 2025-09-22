ae6f9e0fb0bfac4239945869b571babe70b4557a
Holmes555 <Ivan.bazhenov333@gmail.com>
2025-09-22 17:45:09 +0200

    [LeetCode] : 3005. Count Elements With Maximum Frequency.

---
+++ leetcode/3005_Count_Elements_With_Maximum_Frequency.py
@@ -0,0 +1,18 @@
+﻿from typing import List
+
+
+class Solution:
+    def maxFrequencyElements(self, nums: List[int]) -> int:
+        frequency = {}
+        max_freq = 0
+        count_max_freq = 0
+
+        for num in nums:
+            frequency[num] = frequency.get(num, 0) + 1
+            if frequency[num] > max_freq:
+                max_freq = frequency[num]
+                count_max_freq = max_freq
+            elif frequency[num] == max_freq:
+                count_max_freq += max_freq
+
+        return count_max_freq
