c588bd9fca3f92391d7b73dca90bf44c4870cd42
Holmes555 <Ivan.bazhenov333@gmail.com>
2025-09-26 17:07:35 +0200

    [LeetCode] : 611. Valid Triangle Number.

---
+++ leetcode/611_Valid_Triangle_Number.py
@@ -0,0 +1,18 @@
+﻿from typing import List
+
+
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        count = 0
+        n = len(nums)
+
+        for i in range(n - 2):
+            k = i + 2
+            for j in range(i + 1, n - 1):
+                while k < n and nums[i] + nums[j] > nums[k]:
+                    k += 1
+                if k > j:
+                    count += k - j - 1
+
+        return count
