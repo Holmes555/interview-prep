0573f23b8aeee04b2c9bc679d3c86af98029e1d1
Holmes555 <Ivan.bazhenov333@gmail.com>
2025-10-02 14:03:26 +0200

    [LeetCode] : 3100. Water Bottles II.

---
+++ leetcode/3100_Water_Bottles_II.py
@@ -0,0 +1,11 @@
+﻿class Solution:
+    def maxBottlesDrunk(self, numBottles: int, numExchange: int) -> int:
+        total_drunk = numBottles
+        empty_bottles = numBottles
+
+        while empty_bottles >= numExchange:
+            empty_bottles = empty_bottles - numExchange + 1
+            total_drunk += 1
+            numExchange += 1
+
+        return total_drunk
