713c364d4e13207890f5f9ecedcc057a51055cc0
Holmes555 <Ivan.bazhenov333@gmail.com>
2025-10-07 16:56:23 +0200

    [LeetCode] : 1488. Avoid Flood in The City.

---
+++ leetcode/1488_Avoid_Flood_in_The_City.py
@@ -0,0 +1,29 @@
+﻿from typing import List
+
+
+class Solution:
+    def avoidFlood(self, rains: List[int]) -> List[int]:
+        n = len(rains)
+        res = [-2] * n
+        full_lakes = {}
+        dry_days = []
+
+        for i, lake in enumerate(rains):
+            if lake == 0:
+                dry_days.append(i)
+                res[i] = 1
+            else:
+                if lake in full_lakes:
+                    for day in dry_days:
+                        if day > full_lakes[lake]:
+                            res[day] = lake
+                            dry_days.remove(day)
+                            full_lakes.pop(lake)
+                            break
+                    else:
+                        return []
+
+                full_lakes[lake] = i
+                res[i] = -1
+
+        return res
