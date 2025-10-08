a2121bf759c0822c53d92d4cf9487a67d8ef5414
Holmes555 <Ivan.bazhenov333@gmail.com>
2025-10-08 21:45:15 +0200

    [LeetCode] : 2300. Successful Pairs of Spells and Potions.

---
+++ leetcode/2300_Successful_Pairs_of_Spells_and_Potions.py
@@ -0,0 +1,20 @@
+﻿from typing import List
+
+
+class Solution:
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        potions.sort()
+        n = len(potions)
+        res = []
+
+        for spell in spells:
+            left, right = 0, n - 1
+            while left <= right:
+                mid = (left + right) // 2
+                if spell * potions[mid] >= success:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            res.append(n - left)
+
+        return res
