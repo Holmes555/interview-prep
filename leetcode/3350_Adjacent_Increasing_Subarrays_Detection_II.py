09f0535ffb03fd65c8aa863b71b829ec8dba1e1c
Holmes555 <Ivan.bazhenov333@gmail.com>
2025-10-15 18:39:37 +0200

    [LeetCode] : 3350. Adjacent Increasing Subarrays Detection II.

---
+++ leetcode/3350_Adjacent_Increasing_Subarrays_Detection_II.py
@@ -0,0 +1,15 @@
+﻿from typing import List
+
+
+class Solution:
+    def maxIncreasingSubarrays(self, nums: List[int]) -> int:
+        n = len(nums)
+        cur, prev, res = 1, 0, 0
+        for i in range(1, n):
+            if nums[i] > nums[i - 1]:
+                cur += 1
+            else:
+                prev, cur = cur, 1
+            res = max(res, min(prev, cur))
+            res = max(res, cur // 2)
+        return res
