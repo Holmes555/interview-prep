f19ef2deea1bb78553d5708e71e0fc3530f315e1
Holmes555 <Ivan.bazhenov333@gmail.com>
2025-10-18 20:35:15 +0200

    [LeetCode] : 3397. Maximum Number of Distinct Elements After Operations.

---
+++ leetcode/3397_Maximum_Number_of_Distinct_Elements_After_Operations.py
@@ -0,0 +1,15 @@
+﻿from typing import List
+
+
+class Solution:
+    def maxDistinctElements(self, nums: List[int], k: int) -> int:
+        nums = sorted(nums)
+        prev = float('-inf')
+        res = 0
+
+        for n in nums:
+            cur = min(max(prev + 1, n - k), n + k)
+            if prev != cur:
+                res += 1
+                prev = cur
+        return res
