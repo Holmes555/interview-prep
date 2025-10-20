027af9ec185d7f2bd4aabfa1b8c21498e2353d58
Holmes555 <Ivan.bazhenov333@gmail.com>
2025-10-20 14:41:53 +0200

    [LeetCode] : 2011. Final Value of Variable After Performing Operations.

---
+++ leetcode/2011_Final_Value_of_Variable_After_Performing_Operations.py
@@ -0,0 +1,16 @@
+﻿from typing import List
+
+
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        operations_map = {
+            "++X": 1,
+            "X++": 1,
+            "--X": -1,
+            "X--": -1,
+        }
+        result = 0
+
+        for operation in operations:
+            result += operations_map[operation]
+        return result
