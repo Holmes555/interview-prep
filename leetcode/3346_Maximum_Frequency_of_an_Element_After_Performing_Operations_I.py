901720c7972a581124215dd3a2195b1de03bbb2a
Holmes555 <Ivan.bazhenov333@gmail.com>
2025-10-21 22:12:09 +0200

    [LeetCode] : 3346. Maximum Frequency of an Element After Performing Operations I.

---
+++ leetcode/3346_Maximum_Frequency_of_an_Element_After_Performing_Operations_I.py
@@ -0,0 +1,48 @@
+﻿from typing import List
+
+
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int, numOperations: int) -> int:
+        nums.sort()
+        n = len(nums)
+        left = 0
+        total = 0
+        res = 1
+
+        for right in range(1, n):
+            total += (nums[right] - nums[right - 1]) * (right - left)
+
+            while total > k * numOperations:
+                total -= nums[right] - nums[left]
+                left += 1
+
+            res = max(res, right - left + 1)
+
+        return res
+
+
+class Solution2:
+    def maxFrequency(self, nums: List[int], k: int, numOperations: int) -> int:
+        nums.sort()
+        ans = 0
+        num_count = {}
+        last_num_index = 0
+        for i in range(len(nums)):
+            if nums[i] != nums[last_num_index]:
+                num_count[nums[last_num_index]] = i - last_num_index
+                ans = max(ans, i - last_num_index)
+                last_num_index = i
+
+        num_count[nums[last_num_index]] = len(nums) - last_num_index
+        ans = max(ans, len(nums) - last_num_index)
+
+        for i in range(nums[0], nums[-1] + 1):
+            l = bisect.bisect_left(nums, i - k)
+            r = bisect.bisect_right(nums, i + k) - 1
+            if i in num_count:
+                temp_ans = min(r - l + 1, num_count[i] + numOperations)
+            else:
+                temp_ans = min(r - l + 1, numOperations)
+            ans = max(ans, temp_ans)
+
+        return ans
